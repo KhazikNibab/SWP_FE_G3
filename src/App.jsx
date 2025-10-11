689334a32b23b138de7f8af2db1f16b7e4e9f969
Khang <alexthewonder14@gmail.com>
2025-10-11 14:10:13 +0700

    demo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import LoginPage from "./login/LoginPage";
-import "./App.css";
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate,
+// } from "react-router-dom";
+// import LoginPage from "./components/login/LoginPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./components/dashboard";
+import ManageBike from "./pages/bike";
+import ManageCategory from "./pages/category";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />,
+      children: [
+        {
+          path: "bike",
+          element: <ManageBike />,
+        },
+        {
+          path: "category",
+          element: <ManageCategory />,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <RouterProvider router={router} />
+    // <Router>
+    //   <div className="App">
+    //     <Routes>
+    //       <Route path="/" element={<LoginPage />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 }
 
